0ee2d9a12b86ac34ead0eadd6ab976377f5a35f9
Cristian Olaru <colaru@gmail.com>
2012-12-06 19:42:47 +0200

    upgrade to bootstrap-ui is 1.0.RC3 and platform-ui 1.0.RC2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,8 +39,8 @@ grails.project.dependency.resolution = {
         }
         
 		runtime ":jquery:1.7.1"
-        compile ":platform-ui:1.0.M1-SNAPSHOT" 
-        compile ":bootstrap-ui:1.0-SNAPSHOT"
+        compile ":platform-ui:1.0.RC2" 
+        compile ":bootstrap-ui:1.0.RC3"
 		runtime ":twitter-bootstrap:2.2.1"
     }
 }
